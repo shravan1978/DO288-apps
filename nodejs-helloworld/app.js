f06dad15e9c7e6a093a1afe8d81a95a4b53487d5
Student User <student@workstation.lab.example.com>
2020-11-24 21:29:32 -0500

    Fixed json syntax error

--- nodejs-helloworld/app.js
+++ nodejs-helloworld/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!\n');
+  res.send('soap is not formed from foam!\n');
 });
 
 app.listen(8080, function () {
